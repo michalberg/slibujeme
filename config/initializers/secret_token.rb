d557dc1d586094fc0c106e2b77e9c1260bc815fc
Adam Kloboučník <a.kloboucnik@gmail.com>
2012-04-01 19:44:39 +0200

    Initial import of bootstrapped application with cucumber and rspec set up.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slibujeme::Application.config.secret_token = 'a74b0a05b7d7730bd7f90625bddbddabc03c35f804318f13a747120ad10ea8cd107309a6620301bd1fd4153c3ba13953501f205a3372d42d20aa58ee8bbd54f7'
