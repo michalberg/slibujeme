74fd7e9d4076f70bef794698400a579393184bf1
Adam Kloboučník <a.kloboucnik@gmail.com>
2012-04-14 19:29:48 +0200

    Fixed scenario for javascript selected topic.

--- features/step_definitions/application_steps.rb
+++ features/step_definitions/application_steps.rb
@@ -9,3 +9,14 @@ When /^I type in "([^\"]*)" into autocomplete list "([^\"]*)" and I choose "([^\
    sleep 1
    page.driver.browser.execute_script %Q{ $('.ui-menu-item a:contains("#{should_select}")').trigger("mouseenter").trigger("click"); }
 end
+
+When /^I type in "([^\"]*)" into javascript select box "([^\"]*)" and I choose "([^\"]*)"$/ do |typed, input_name,should_select|
+   page.driver.browser.execute_script %Q{ $('li.search-field input').trigger("focus") }
+   # fill_in("#{input_name}",:with => typed)
+   page.driver.browser.execute_script %Q{ 
+     $("label:contains('#{input_name}')").siblings().find("li.search-field input").val("#{typed}") 
+    }
+   page.driver.browser.execute_script %Q{ $('li.search-field input').trigger("keydown") }
+   sleep 1
+   page.driver.browser.execute_script %Q{ $('.chzn-results li:contains("#{should_select}")').trigger("mouseenter").trigger("click").trigger("mousedown").trigger("mouseup"); }
+end
--- features/step_definitions/topic_steps.rb
+++ features/step_definitions/topic_steps.rb
@@ -3,3 +3,10 @@ Then /^there should be topic "([^"]*)" with children:$/ do |title, children|
   @topic = Topic.find_by_title!(title)
   @topic.children.map{ |child| {"title" => child.title} }.should =~ children.hashes
 end
+
+Given /^there are the following topics:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  table.hashes.each do |attributes|
+    Topic.create!(attributes)
+  end
+end
