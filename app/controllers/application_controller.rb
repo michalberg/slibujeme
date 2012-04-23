8f8dca9d24907d91095b654d4eb1e16132eb71ba
Adam Kloboučník <a.kloboucnik@gmail.com>
2012-04-23 17:35:55 +0200

    Implemented simple 404 page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from ActionController::RoutingError, :with => :render_404
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+  
+  private
+  def render_404
+    render :template => "application/render_404", :status => :not_found
+  end
 end
