ec52a9db0987ce36bdbf41f5e6c5c31e1ed29b62
Adam Kloboučník <a.kloboucnik@gmail.com>
2012-05-04 15:48:11 +0200

    Added controller helper for getting last 10 materials and used in API.

--- app/controllers/api/v1/materials_controller.rb
+++ app/controllers/api/v1/materials_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::MaterialsController < Api::V1::BaseController
   def index
     respond_with(
-      Material.order("created_at DESC").last(10).to_json(
+      get_last_added_materials.to_json(
         :only => [:id], 
         :include => { 
           :image_assets => { :methods => :image_url, :only => [:id, :image_url] }, 
