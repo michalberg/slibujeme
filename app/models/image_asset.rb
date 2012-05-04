56edbd67c66a362310ae15678d46bba9b7648def
Adam Kloboučník <a.kloboucnik@gmail.com>
2012-05-04 11:28:34 +0200

    Added image assets mime type validation.

--- app/models/image_asset.rb
+++ app/models/image_asset.rb
@@ -1,7 +1,7 @@
 class ImageAsset < ActiveRecord::Base
   belongs_to :material
   has_attached_file :image, :styles => { :thumb => "220x220!" , :detail => "800x1200>" }
-
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/x-png', 'image/gif', 'application/pdf']
   def image_url(size = :thumb)
     image.url(size)
   end
