5d88aa76d52a5b53bd365d6970def0974d3eaabc
Adam Kloboučník <a.kloboucnik@gmail.com>
2012-05-28 10:42:37 +0200

    Changed url_assets url field to text.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120528073543) do
+ActiveRecord::Schema.define(:version => 20120528082437) do
 
   create_table "elections", :force => true do |t|
     t.string   "title"
@@ -95,7 +95,7 @@ ActiveRecord::Schema.define(:version => 20120528073543) do
   end
 
   create_table "url_assets", :force => true do |t|
-    t.string   "url"
+    t.text     "url"
     t.integer  "material_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
